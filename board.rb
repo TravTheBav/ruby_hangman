187bcfab93656483b4fc7134cc93a1fcfed304e6
Travis <tzito93@gmail.com>
2022-03-10 10:45:10 -0600

    Added Board class with stick figure drawing

---
+++ board.rb
@@ -0,0 +1,24 @@
+# A class representing a board in hangman
+# Responsible for drawing the stick figure and displaying text on each turn
+
+class Board
+  def initialize
+    @body_parts = [  
+      "_________ ",
+      "        | ",
+      "        | ",
+      "        O ",
+      "       /|\\ ",
+      "       / \\ ",
+    ]
+    @current_row = 0
+  end
+  
+  def render_stick_figure
+    @body_parts.each_with_index do |part, idx|
+      break if idx == @current_row
+      
+      puts part
+    end
+  end
+end
